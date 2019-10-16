4e42143f09ff9a1a33a045e29662722227e55870
CortanaCorp <56658305+CortanaCorp@users.noreply.github.com>
2019-10-16 18:32:43 -0400

    Add files via upload

---
+++ upgrade_cortana/README.txt.txt
@@ -0,0 +1,2 @@
+Run __init__.bat
+Then run cortana_multiple_features.bat
---
+++ upgrade_cortana/__init__.bat
@@ -0,0 +1,5 @@
+$git clone repo @ *address*4rt::6rE*comment*(C) 2015 Paul Zyberback 
+REM	!!This will create a hidden file called cort.config*
+$git add commandPckge id:isa91idu9as8918.29usd928@github.com
+$cd *arbritpcname[recieve&fromUser]*\downloads\82hsa81jksda91jfhg0a2ogl::189ajs919694520\hidden\cort.config
+$key.hh/hit."ENTER&stop0"
